4890a20a3253f598ef379243a8e248fd39ad054f
ganesh9803 <santhosh.ganesh1023@gmail.com>
2025-01-22 12:28:47 +0530

    u

--- src/components/ResumeButton.tsx
+++ src/components/ResumeButton.tsx
@@ -3,7 +3,7 @@ import { FileDown } from 'lucide-react';
 export function ResumeButton() {
   return (
     <a
-      href="https://drive.google.com/uc?export=download&id=1nySxpbS94EpDF17LQcbbsDwE99gJTVuw"
+      href="https://drive.google.com/file/d/1u0ogpSWfAOSQxh9abz3wTT1Surh6Wjol/view?usp=drive_link"
       download="Ganesh-Santhosh-Resume.pdf"
       className="flex items-center px-4 py-2 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700 transition-colors"
     >
