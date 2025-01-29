af9fdbb67e2910e75cbcaa6fc4b6a02a8dc1c0ed
ganesh9803 <santhosh.ganesh1023@gmail.com>
2025-01-29 12:03:53 +0530

    up

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -17,12 +17,6 @@ export function About() {
             <p className="text-lg text-gray-600 dark:text-gray-300 mb-6">
             As an aspiring full-stack developer with a solid technical background, I possess a robust foundation in Full Stack Development with specialized expertise in the MERN stack (MongoDB, Express, React, Node.js). My dedication lies in crafting seamless and scalable web solutions that redefine user experiences.
             </p>
-            <p className="text-lg text-gray-600 dark:text-gray-300 mb-6">
-            I am deeply passionate about driving digital transformation and leveraging technology to innovate across industries.I am eager to secure a challenging position in the tech industry where I can apply my technical expertise and problem-solving skills to contribute meaningfully to dynamic development teams. My goal is to engage in impactful projects that push boundaries and set new benchmarks in technology innovation. I aspire to grow professionally by learning from experienced professionals and embracing cutting-edge technologies that shape the future of digital solutions.
-            </p>
-            <p className="text-lg text-gray-600 dark:text-gray-300">
-            My drive stems from a deep-seated passion for technology and its potential to drive positive change. I thrive in collaborative environments where creativity and innovation are encouraged, eager to contribute fresh perspectives and valuable insights to achieve collective goals.
-            </p>
           </div>
         </div>
       </div>
--- src/components/ResumeButton.tsx
+++ src/components/ResumeButton.tsx
@@ -3,7 +3,7 @@ import { FileDown } from 'lucide-react';
 export function ResumeButton() {
   return (
     <a
-      href="https://drive.google.com/file/d/1u0ogpSWfAOSQxh9abz3wTT1Surh6Wjol/view?usp=drive_link"
+      href="https://drive.google.com/file/d/1_jYJEwbdLHVZQgyPto_6VWhjgS7wSXF_/view?usp=sharing"
       download="Ganesh-Santhosh-Resume.pdf"
       className="flex items-center px-4 py-2 bg-indigo-600 text-white rounded-lg hover:bg-indigo-700 transition-colors"
     >
